7f260f3768ed4e856a1da5119b3f5845ecebef00
Dinh Mahone <40380462+dinhnt12@users.noreply.github.com>
2022-10-12 13:04:55 +0700

    Update SEN contract (#1)

--- contracts/SensparkToken.sol
+++ contracts/SensparkToken.sol
@@ -1,57 +1,34 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.4;
 
-import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20BurnableUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/security/PausableUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
-import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract SensparkToken is
-  Initializable,
-  ERC20Upgradeable,
-  ERC20BurnableUpgradeable,
-  AccessControlUpgradeable,
-  PausableUpgradeable,
-  UUPSUpgradeable
-{
-  bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
-  bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
-  bytes32 public constant UPGRADER_ROLE = keccak256("UPGRADER_ROLE");
+contract SensparkToken is ERC20, Pausable, Ownable {
+    constructor() ERC20("Senspark", "SEN") {}
 
-  function initialize() public initializer {
-    __ERC20_init("Senspark", "SEN");
-    __ERC20Burnable_init();
-    __Pausable_init();
-    __AccessControl_init();
-    __UUPSUpgradeable_init();
+    function pause() public onlyOwner {
+        _pause();
+    }
 
-    _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
-    _grantRole(PAUSER_ROLE, msg.sender);
-    _grantRole(MINTER_ROLE, msg.sender);
-    _grantRole(UPGRADER_ROLE, msg.sender);
-  }
+    function unpause() public onlyOwner {
+        _unpause();
+    }
 
-  function pause() public onlyRole(PAUSER_ROLE) {
-    _pause();
-  }
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
 
-  function unpause() public onlyRole(PAUSER_ROLE) {
-    _unpause();
-  }
+    function burn(uint256 amount) external {
+        _burn(msg.sender, amount);
+    }
 
-  function mint(address to, uint256 amount) public onlyRole(MINTER_ROLE) {
-    _mint(to, amount);
-  }
-
-  function _beforeTokenTransfer(
-    address from,
-    address to,
-    uint256 amount
-  ) internal override whenNotPaused {
-    super._beforeTokenTransfer(from, to, amount);
-  }
-
-  function _authorizeUpgrade(address newImplementation) internal override onlyRole(UPGRADER_ROLE) {}
+    function _beforeTokenTransfer(address from, address to, uint256 amount)
+        internal
+        whenNotPaused
+        override
+    {
+        super._beforeTokenTransfer(from, to, amount);
+    }
 }
